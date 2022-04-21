1ccdd1afae0600f26a5d97eb6f43a47e9b785583
Davi Mateus <davi.2020109493@unicap.br>
2022-04-21 16:03:16 -0300

    Update main.js

--- deploy/main.js
+++ deploy/main.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
