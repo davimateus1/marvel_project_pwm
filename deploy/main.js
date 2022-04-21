010de25d84cd7d84f1073620d10852b09454529e
Davi Mateus <davi.2020109493@unicap.br>
2022-04-21 15:43:51 -0300

    teste deploy heroku

---
+++ deploy/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
