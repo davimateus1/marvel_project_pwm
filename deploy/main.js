52eb1855fdad2bb190545da0e47a93b5222d6b20
Davi Mateus <davi.2020109493@unicap.br>
2022-04-30 23:23:13 -0300

    Update main.js

--- deploy/main.js
+++ deploy/main.js
@@ -12,8 +12,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
